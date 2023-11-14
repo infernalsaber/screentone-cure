1c87efb04e2afdb8a454e511d2ff5219742000c5
infernalsaber <72517486+infernalsaber@users.noreply.github.com>
2023-11-14 14:13:35 +0530

    streamlit issues fix attempt (another)

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import os
 os.system("apt-get update && apt-get install libgl1")
 
 import streamlit as st
-import cv2
+from cv2 import GaussianBlur, bilateralFilter, filter2D, imdecode, IMREAD_COLOR
 import numpy as np
 from io import BytesIO
 
@@ -14,21 +14,21 @@ versionNumber = '0.1'
 
 def blur(img, blur_amount=5):
     if blur_amount == 7:
-        dst2 = cv2.GaussianBlur(img, (7, 7), 0)
-        dst = cv2.bilateralFilter(dst2, 7, 80, 80)
+        dst2 = GaussianBlur(img, (7, 7), 0)
+        dst = bilateralFilter(dst2, 7, 80, 80)
     else:
-        dst2 = cv2.GaussianBlur(img, (5, 5), 0)
-        dst = cv2.bilateralFilter(dst2, 7, 10 * blur_amount, 80)
+        dst2 = GaussianBlur(img, (5, 5), 0)
+        dst = bilateralFilter(dst2, 7, 10 * blur_amount, 80)
     return dst
 
 def sharp(img, sharp_point, sharp_low):
     s_kernel = np.array([[0, sharp_low, 0], [sharp_low, sharp_point, sharp_low], [0, sharp_low, 0]])
-    sharpened = cv2.filter2D(img, -1, s_kernel)
+    sharpened = filter2D(img, -1, s_kernel)
     return sharpened
 
 def remove_screentones(input_image, blur_amount=2, sh_point=5.56, sh_low=-1.14):
     img_array = np.asarray(bytearray(input_image.read()), dtype=np.uint8)
-    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+    img = imdecode(img_array, IMREAD_COLOR)
     blurred = blur(img, blur_amount)
     ret = sharp(blurred, sh_point, sh_low)
     return ret
